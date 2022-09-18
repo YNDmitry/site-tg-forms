c1f3c206724109494d323c457d0e4382e2d4bc02
Dmitry <60594670+YNDmitry@users.noreply.github.com>
2022-09-18 11:03:52 +0500

    cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: process?.env?.ORIGIN || 'http://localhost:3000',
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
 }
